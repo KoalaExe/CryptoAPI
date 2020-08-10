c5974d723a84a7900c53be7dc5358e87bc87583d
Marshall <marshall@Charleys-MacBook-Air.local>
2020-08-11 00:19:25 +1000

    Initial commit

---
+++ src/test/java/com/dev/CryptoAPI/CryptoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.CryptoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
